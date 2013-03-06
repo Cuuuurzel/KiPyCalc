18ce0b1ec5767afecbd26f8c64e471f30f8c8501
Cuuuurzel <fcurzel94@gmail.com>
2013-03-06 18:05:27 +0100

    Fix exit on return key

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ from kivy.app import App
 from kivy.base import EventLoop
 from kivy.config import Config
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.settings import Settings
 from shell import *
 from plotter import *
 from kivyextras import *
+
 
 class KiPyCalc( BoxLayout ) :
 
@@ -42,6 +44,8 @@ class KiPyCalc( BoxLayout ) :
             self.clear_widgets()
             self.add_widget( self.shell )
             self.plottingOptionPanel.dismiss( True )
+            return True
+        else : return False
 
 
 class KiPyCalcApp( App ) : 
@@ -50,17 +54,24 @@ class KiPyCalcApp( App ) :
     title = 'KiPyCalc'
     
     def build( self ) :
+        
         self.kpc = KiPyCalc()
         self.kpc.start()
         EventLoop.window.bind( on_keyboard=self.hook_keyboard )
         return self.kpc
+        """
+        s = Settings()
+        s.add_json_panel('My custom panel', {}, 'settings_custom.json')
+        #s.add_json_panel('Another panel', config, 'settings_test2.json')
+        return s
+        """
 
     def hook_keyboard( self, window, key, *largs ):
         if key == 27 :
-            self.kpc.onReturnKey() 
-            return True 
+            return self.kpc.onReturnKey() 
 
-    def on_pause( self ) : return True
+    def on_pause( self ) : 
+         return True
 
 if __name__ in [ "__android__", "__main__" ] :
     KiPyCalcApp().run()
